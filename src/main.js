e0efd040fe5df41139c017d0d16dab5e8ae5896d
danny <khc741213@gmail.com>
2022-06-06 23:24:47 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,21 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { createRouter,createWebHistory } from 'vue-router'
-import Home from './views/Home.vue'
 
-const router = createRouter({ 
-    history:createWebHistory(),
-    routes:[
-        {
-            path:'/',
-            component:Home
-        },
-        {
-            path:'/about',
-            component:()=>import('./views/About.vue')
-        }
-    ]
- })
 
 createApp(App).use(router).mount('#app')
